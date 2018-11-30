92838b6c9d6ed66fd45d87f25867e697f52920a9
Ove Danner <ovedanner@gmail.com>
2018-11-30 17:41:00 +0100

    Fixed returning html from Redis

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -1,6 +1,6 @@
 class FrontendController < ActionController::API
   def index
     html = Redis.current.get('robots:index:current-content')
-    render text: html
+    render plain: html, content_type: 'text/html'
   end
 end
--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Connect to the repository.
 $(aws ecr get-login)
 
 # Build the image for production
-docker build --file ./docker/app/Dockerfile --build-arg bundle_install_args="--without development test" -t production .
+sudo docker build --file ./docker/app/Dockerfile --build-arg bundle_install_args="--without development test" -t production .
 
 # Tag the image.
-docker tag production:latest 764162651181.dkr.ecr.eu-west-1.amazonaws.com/robots/production
+sudo docker tag production:latest 764162651181.dkr.ecr.eu-west-1.amazonaws.com/robots/production
 
 # Push the image.
-docker push 764162651181.dkr.ecr.eu-west-1.amazonaws.com/robots/production
+sudo docker push 764162651181.dkr.ecr.eu-west-1.amazonaws.com/robots/production
 
 # Tell the service to redeploy.
-aws ecs update-service --cluster robots-cluster --service robots-service --force-new-deployment
+aws ecs update-service --force-new-deployment --cluster robots-cluster --service robots-service
