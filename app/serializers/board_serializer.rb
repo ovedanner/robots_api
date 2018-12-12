3d56102772a361661b726405950c0a0603761a01
Ove Danner <ovedanner@gmail.com>
2018-12-12 18:52:52 +0100

    Some light cleanup

--- app/serializers/board_serializer.rb
+++ app/serializers/board_serializer.rb
@@ -1,15 +1,3 @@
 class BoardSerializer < ActiveModel::Serializer
-  attributes :id
-
-  attribute :cells do
-    object.cells
-  end
-
-  attribute :goals do
-    object.goals
-  end
-
-  attribute :robot_colors do
-    object.robot_colors
-  end
+  attributes :id, :cells, :goals, :robot_colors
 end
